d0a95d3d8b75e6b3e328397a6544b290d24be56f
amoney79 <mackenziejoseph396@gmail.com>
2026-09-09 21:30:01 +0300

    Entity,Table,Id, generated value annotations added

--- src/main/java/com/amoney/studentManagementSystem/entity/student.java
+++ src/main/java/com/amoney/studentManagementSystem/entity/student.java
@@ -1,6 +1,16 @@
 package com.amoney.studentManagementSystem.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
 public class student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
